b45c1c3be06cb5e8042fb769f6ed818814f1f677
Atthar Mirza <atthar.mirza@gmail.com>
2017-05-09 16:55:44 -0400

    Journal Prototype Functioning
    The journal page is running properly, with a progress bar that adjusts
    according to the current week.

--- journal/journal.js
+++ journal/journal.js
@@ -1,14 +1,54 @@
 $(document).ready(function () {
-    
-    $('.pageButton').fadeIn(1000);
-    
-    var isHovering = $('.pageButton').mouseenter(function () {
-        console.log('yay');
-        return true;
+    //navigation buttons appearing and fading
+    $('.pageButton').fadeIn(1000).delay(5000).fadeOut(500);
+    $('.hitbox').mouseenter(function () {
+        $('.pageButton').stop(true).fadeIn(100);
     });
+    $('.hitbox').mouseleave(function () {
+        $('.pageButton').stop(true).fadeOut(100);
+    });
+    
+    //recording click events
+    
+    var totalLogs = 22;
+    var currentLog = 0;
+    var progressPercent = 0; 
+    
+    function progressBar() {
+        progressPercent = currentLog/totalLogs*100;
+        return String(progressPercent + '%');
+    };
+    
+    $('#nextPage').click(function () {
+        nextLog(1);
+        console.log(progressBar());
         
-    var isHovering = $('.pageButton').mouseleave(function () {
-        console.log('nay');
-        return false;
+        $('#progressBar').width(progressBar());
     });
+    
+    function nextLog(i) {
+        if (currentLog < totalLogs) {
+            currentLog = currentLog + i;
+        } else {
+            currentLog;
+        };
+        console.log(currentLog);
+    }
+    
+    $('#lastPage').click(function () {
+        lastLog(1);
+        console.log(progressBar());
+        
+        $('#progressBar').width(progressBar());
+    });
+    
+    function lastLog(i) {
+        if (currentLog > 0) {
+            currentLog = currentLog - i;
+        } else {
+            currentLog;
+        };
+        console.log(currentLog);
+    }
+    
 });
